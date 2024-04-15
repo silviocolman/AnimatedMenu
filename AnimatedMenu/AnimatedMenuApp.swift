dcc552ab4889525531c57a04a2d6efc337416765
Silvio Colman <silviorafael.colman@gmail.com>
2024-04-15 14:37:01 -0400

    Initial Commit

---
+++ AnimatedMenu/AnimatedMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedMenuApp.swift
+//  AnimatedMenu
+//
+//  Created by Silvio Colmán on 2024-04-15.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
