5d8ed345245d8144a908e66491ef93cfe58e91c0
Silvio Colman <silviorafael.colman@gmail.com>
2024-04-16 21:55:32 -0400

    Animated Side Menu

---
+++ AnimatedMenu/Helpers/AnimatedSideBar.swift
@@ -0,0 +1,18 @@
+//
+//  AnimatedSideBar.swift
+//  AnimatedMenu
+//
+//  Created by Silvio Colmán on 2024-04-15.
+//
+
+import SwiftUI
+
+struct AnimatedSideBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimatedSideBar()
+}
