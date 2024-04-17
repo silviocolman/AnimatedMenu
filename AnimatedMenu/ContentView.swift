c58841d0be4f5cea90179d810e37a780c56bf680
Silvio Colman <silviorafael.colman@gmail.com>
2024-04-16 22:02:27 -0400

    Proyecto Completado

--- AnimatedMenu/ContentView.swift
+++ AnimatedMenu/ContentView.swift
@@ -8,14 +8,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    /// View Properties
+    @State private var showMenu: Bool = false
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        AnimatedSideBar(
+            rotateWhenExpands: true,
+            disableIteraction: true,
+            sideMenuWidth: 200,
+            cornerRadius: 25,
+            showMenu: $showMenu
+        ) { safeArea in
+            NavigationStack {
+                List {
+                    NavigationLink("Detail View") {
+                        Text("Hello Silvio")
+                            .navigationTitle("Detail")
+                    }
+                }
+                .navigationTitle("Home")
+                .toolbar {
+                    ToolbarItem(placement: .topBarLeading) {
+                        Button(action: { showMenu.toggle()}, label: {
+                            Image(systemName: showMenu ? "xmark" : "line.3.horizontal")
+                                .foregroundColor(Color.primary)
+                                .contentTransition(.symbolEffect)
+                        })
+                    }
+                }
+            }
+        } menuView: { safeArea in
+            SideBarMenuView(safeArea)
+        } background: {
+            Rectangle()
+                .fill(.sideMenu)
         }
-        .padding()
+    }
+    
+    @ViewBuilder
+    func SideBarMenuView(_ safeArea: UIEdgeInsets) -> some View {
+        VStack(alignment: .leading, spacing: 12) {
+           Text("Side Menu")
+                .font(.largeTitle.bold())
+                .padding(.bottom, 10)
+            
+            SideBarButton(.home)
+            SideBarButton(.bookmark)
+            SideBarButton(.favourites)
+            SideBarButton(.profile)
+            
+            Spacer()
+            
+            SideBarButton(.logout)
+        }
+        .padding(.horizontal, 15)
+        .padding(.vertical, 20)
+        .padding(.top, safeArea.top)
+        .padding(.bottom, safeArea.bottom)
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+        .environment(\.colorScheme, .dark)
+    }
+    
+    @ViewBuilder
+    func SideBarButton(_ tab: Tab, onTap:@escaping () -> () = {  }) -> some View{
+        Button(action: onTap, label: {
+            HStack(spacing: 12) {
+                Image(systemName: tab.rawValue)
+                    .font(.title3)
+                
+                Text(tab.title)
+                    .font(.callout)
+                
+                Spacer(minLength: 0)
+            }
+            .padding(.vertical, 10)
+            .contentShape(.rect)
+            .foregroundColor(Color.primary)
+        })
+    }
+    
+    /// Sample Tab's
+    enum Tab: String, CaseIterable {
+        case home = "house.fill"
+        case bookmark = "book.fill"
+        case favourites = "heart.fill"
+        case profile = "person.crop.circle"
+        case logout = "rectangle.portrait.and.forward.fill"
+        
+        var title: String {
+            switch self {
+            case .home: return "Home"
+            case .bookmark: return "Bookmark"
+            case .favourites: return "Favourites"
+            case .profile: return "Profile"
+            case .logout: return "Logout"
+            }
+        }
     }
 }
 
--- AnimatedMenu/Helpers/AnimatedSideBar.swift
+++ AnimatedMenu/Helpers/AnimatedSideBar.swift
@@ -7,12 +7,134 @@
 
 import SwiftUI
 
-struct AnimatedSideBar: View {
+struct AnimatedSideBar<Content: View, MenuView: View, Background: View>: View {
+    
+    /// Customization Options
+    var rotateWhenExpands: Bool = true
+    var disableIteraction: Bool = true
+    var sideMenuWidth: CGFloat = 200
+    var cornerRadius: CGFloat = 25
+    
+    @Binding var showMenu: Bool
+    
+    @ViewBuilder var content: (UIEdgeInsets) -> Content
+    @ViewBuilder var menuView: (UIEdgeInsets) -> MenuView
+    @ViewBuilder var background: Background
+    
+    /// View Properties
+    @GestureState private var isDragging: Bool = false
+    @State private var offsetX: CGFloat = 0
+    @State private var lastOffsetX: CGFloat = 0
+    
+    /// Used to Dim Content View Wen Side Bar is Being Dragged
+    @State private var progress: CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader {
+            let size = $0.size
+            let safeArea = (UIApplication.shared.connectedScenes.first as? UIWindowScene)?.keyWindow?.safeAreaInsets ?? .zero
+            
+            HStack(spacing: 0){
+                GeometryReader { _ in
+                    menuView(safeArea)
+                }
+                .frame(width: sideMenuWidth)
+                
+                /// Clipping Menu Interaction Beyond it's Width
+                .contentShape(.rect)
+                
+                GeometryReader { _ in
+                    content(safeArea)
+                }
+                .frame(width: size.width)
+                .overlay {
+                    if disableIteraction && progress > 0 {
+                        Rectangle()
+                            .fill(.black.opacity(progress * 0.2))
+                            .onTapGesture {
+                                withAnimation(.snappy(duration: 0.3, extraBounce: 0)) {
+                                    reset()
+                                }
+                            }
+                    }
+                }
+                .mask {
+                    RoundedRectangle(cornerRadius: progress * cornerRadius)
+                }
+                .scaleEffect(rotateWhenExpands ? 1 - (progress * 0.1) : 1, anchor: .trailing)
+                .rotation3DEffect(
+                    .init(degrees: rotateWhenExpands ? (progress * -15) : 0),
+                                          axis: (x: 0.0, y: 1.0, z: 0.0)
+                )
+            }
+            .frame(width: size.width + sideMenuWidth, height: size.height)
+            .offset(x: -sideMenuWidth)
+            .offset(x: offsetX)
+            .contentShape(.rect)
+            .simultaneousGesture(dragGesture)
+        }
+        .background(background)
+        .ignoresSafeArea()
+        .onChange(of: showMenu) { oldValue, newValue in
+            withAnimation(.snappy(duration: 0.3, extraBounce: 0)) {
+                if newValue {
+                    showSideBar()
+                } else {
+                    reset()
+                }
+            }
+        }
+    }
+    
+    /// Drag Gesture
+    var dragGesture: some Gesture {
+        DragGesture()
+            .updating($isDragging) { _, out, _ in
+                out = true
+            }.onChanged { value in
+                guard value.startLocation.x > 10 else { return }
+                
+                let translationX = isDragging ? max(min(value.translation.width + lastOffsetX, sideMenuWidth), 0) : 0
+                offsetX = translationX
+                calculateProgress()
+            }.onEnded { value in
+                guard value.startLocation.x > 10 else { return }
+                
+                withAnimation(.snappy(duration: 0.3, extraBounce: 0)) {
+                    let velocityX = value.velocity.width / 8
+                    let total = velocityX + offsetX
+                    
+                    if total > (sideMenuWidth * 0.5) {
+                        showSideBar()
+                    } else {
+                        reset()
+                    }
+                }
+            }
+    }
+    
+    /// Show's Side Bar
+    func showSideBar() {
+        offsetX = sideMenuWidth
+        lastOffsetX = offsetX
+        showMenu = true
+        calculateProgress()
+    }
+    
+    /// Reset's to it's Initial State
+    func reset() {
+        offsetX = 0
+        lastOffsetX = 0
+        showMenu = false
+        calculateProgress()
+    }
+    
+    /// Convert's Offset into Series of progress ranging from 0 - 1
+    func calculateProgress() {
+        progress = max(min(offsetX / sideMenuWidth, 1), 0)
     }
 }
 
 #Preview {
-    AnimatedSideBar()
+    ContentView()
 }
